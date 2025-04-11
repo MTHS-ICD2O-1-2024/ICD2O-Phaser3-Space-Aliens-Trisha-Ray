cf2403cde37c81566f74049560e93131d740bdfb
Trisha-Ray <trisha.ray@stu.ocsb.ca>
2025-04-11 17:10:26 +0000

    game js

---
+++ js/game.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Trisha ray
+// Created on: April 2025
+// This file contains the JS functions for index.html
+/*global Phaser*/
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a,
+}
+
+const game= new Phaser.Game(config)
+console.log(game)
