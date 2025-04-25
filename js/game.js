fd8f2e4c7e6e0977b9f01b9e3d1731e9d7371663
Trisha-Ray <trisha.ray@stu.ocsb.ca>
2025-04-25 17:20:25 +0000

    splashScene commited

--- js/game.js
+++ js/game.js
@@ -18,14 +18,14 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a,
+  backgroundColor: 0xffffff,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
 }
 
-const game= new Phaser.Game(config)
+const game = new Phaser.Game(config)
 
   //load scenes
   //NOTE: remember any "key" is global and CAN NOT be reused!
--- js/splashScene.js
+++ js/splashScene.js
@@ -9,22 +9,29 @@
 class SplashScene extends Phaser.Scene {
   constructor () { 
     super({ key: 'splashScene' })
+    this.splashSceneBackgroundImage = null
   }
 
-init (data) {
-  this.cameras.main.setBackgroundColor('#ffffff')
-}
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
 
   preload () {
     console.log('Splash Scene')
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
   }
 
   creat (data) {
+    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
   update (time, delta) {
-    this.scene.switch('titleScene')
+    //if (time > 3000) {
+      //this.scene.switch('titleScene')
+    //}
   }
 }
 
-  export default SplashScene
+export default SplashScene
